0674b6aa4c3d962ba1211a8ff295daf867c34cd3
lucas3322 <lucass706@hotmail.com>
2021-07-23 10:39:27 -0300

    consultas de dados na api

--- src/components/forms/DropdowBrazilianState/index.js
+++ src/components/forms/DropdowBrazilianState/index.js
@@ -1,17 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchStates } from '../../../helpers/Ibge';
 
 const DropdowBrazilianStates = () => {
 
-useEffect(() =>{
-    fetchStates().then(console.log)
-}, []);
+    const [states, setStates] = useState([]);
+    useEffect(() =>{
+        fetchStates().then((states)=> {
+            setStates(states);
+        })
+    }, []);
 
 
     
     return (
         <select id="state">
             <option value="">Selecione um estado</option>
+            {states.map((state)=>{
+                const {sigla, nome} = state;
+                return (<option key={sigla} value={sigla}>{nome}</option>)
+            })}
         </select>
     );
 };
